8de713fd476a372eac878f4810bb844c2a51095c
Fayis KV <mohammedfayis135@gmail.com>
2026-08-08 17:40:57 +0530

    installed needed dependencies

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
+
+  server: {
+    host: true,
+    port: 5173,
+  },
+
+  preview: {
+    host: true,
+    port: 4173,
+  },
+
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
